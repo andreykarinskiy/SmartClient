2c4c60f1f22a8da56e093b999541346012a5688a
Каринский А. <karinskiy.a@helix.ru>
2016-04-20 13:08:08 +0300

    change ioc container to Unity

--- SmartClient/Shell/App.xaml.cs
+++ SmartClient/Shell/App.xaml.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace Shell
+﻿namespace Shell
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
+    using System;
+    using System.Windows;
+
     public partial class App : Application
     {
+        private readonly ShellBootsrapper bootstrapper;
+
+        public App()
+        {
+            bootstrapper = new ShellBootsrapper();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                bootstrapper.Run();
+            }
+            catch (Exception exception)
+            {
+                HandleError(exception);
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            CleanupAndExit(fail: false);
+        }
+
+        private void HandleError(Exception exception)
+        {
+            ReportError(exception);
+
+            CleanupAndExit(fail: true);
+        }
+
+        private static void ReportError(Exception exception)
+        {
+            //TODO: Вызов модального диалогового окна или запись в журнал.
+        }
+
+        private void CleanupAndExit(bool fail)
+        {
+            bootstrapper.Dispose();
+
+            if (fail)
+            {
+                Environment.FailFast("Application initialization failed.");
+            }
+        }
     }
 }
--- SmartClient/Shell/ViewModels/ShellViewModel.cs
+++ SmartClient/Shell/ViewModels/ShellViewModel.cs
@@ -2,7 +2,20 @@
 {
     using Core;
 
+    using Prism.Logging;
+    using Prism.Mvvm;
+
     public class ShellViewModel : ViewModel
     {
+        private readonly ILoggerFacade logger;
+
+        public ShellViewModel(ILoggerFacade logger)
+        {
+            this.logger = logger;
+
+            Title = "Preved!";
+        }
+
+        public string Title { get; set; }
     }
 }
